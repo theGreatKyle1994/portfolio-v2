c199aab542a5d69c954326dfaafcb8b96fc41d86
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-11-06 19:52:22 -0500

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: [
+    "gif",
+    "apng",
+    "png",
+    "jpe?g",
+    "jfif",
+    "pjpeg",
+    "pjp",
+    "gif",
+    "svg",
+    "ico",
+    "webp",
+    "avif",
+  ],
 });
