1eb0dcb8fbe9baa1b07dac8fc1a62325489a6d2f
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-11-10 21:11:11 -0500

    first intersection observer code

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -1,9 +1,27 @@
 import "./Card.css";
 import "./BottomBorder.css";
+import { useRef, useEffect } from "react";
 
 const Card = (props) => {
+  const thisCard = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          console.log(entries[0].target);
+        }
+      },
+      { threshold: 0.25 }
+    );
+    observer.observe(thisCard.current);
+    return () => {
+      observer.unobserve(thisCard.current);
+    };
+  }, [thisCard.current]);
+
   return (
-    <section className={"cardContainer bottomBorder"}>
+    <section className={"cardContainer bottomBorder"} ref={thisCard}>
       {!props.noRender && (
         <h2 className={"cardTitle"}>
           <span className={"cardTitleBracket"}>{"{"}</span>
