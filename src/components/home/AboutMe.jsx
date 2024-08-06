50dac9a8103d21ce410c9c43ff4e683325ee20fe
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-08-06 19:38:21 -0400

    update about me

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -7,8 +7,8 @@ const AboutMe = () => {
     <Card title={"About Me"}>
       <section className={"aboutMeContainer"}>
         <p className={"aboutMe"}>
-          As a Full-Stack Engineer with experience encompassing 4 years of
-          freelance development and over 10 years of client services, I approach
+          As a Full-Stack Engineer with experience encompassing 5+ years in
+          software development and over 10 years of client services, I approach
           projects with the goal of utilizing and adapting innovative
           technologies to perform efficient and aesthetically pleasing functions
           consistent with my clients' vision. I view each project as an
@@ -16,14 +16,14 @@ const AboutMe = () => {
           comply with the lastest industry trends, but also to foster strong
           connections with both clients and colleagues. Prior to Graduating from
           the Coding Dojo in 2023, I assumed the role of mentoring my peers to
-          facilitate mutual development. As a current Open-Source Project Lead,
-          I similarly engage my colleagues in one-on-one code review in addition
-          to hosting Agile based meetings to ensure adherence to project
-          deadlines and to discuss continuous growth. My personal skill set
-          utilizes several essential technologies in the production pipeline,
-          including: JavaScript, TypeScript, Python and a specialty in React. I
-          look forward to assisting you in developing a unique web identity for
-          your brand, special event, organization, or yourself!
+          facilitate mutual development. As a current Project Lead, I similarly
+          engage my colleagues in one-on-one code review in addition to hosting
+          Agile based meetings to ensure adherence to project deadlines and to
+          discuss continuous growth. My personal skill set utilizes several
+          essential technologies in the production pipeline, including:
+          JavaScript, TypeScript, Python and a specialty in React. I look
+          forward to assisting you in developing a unique web identity for your
+          brand, special event, organization, or yourself!
         </p>
         <a
           target={"_blank"}
